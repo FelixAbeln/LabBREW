54394cf4640677cdd670d9dc9b2e036aa3a55cd2
Felix Abeln <felix@familie-abeln.de>
2026-03-23 21:04:06 +0100

    Change service port from 8700 to 8769

--- run_service_data.py
+++ run_service_data.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     sys.argv += ["--backend-host", "127.0.0.1"]
     sys.argv += ["--backend-port", "8765"]
     sys.argv += ["--host", "127.0.0.1"]
-    sys.argv += ["--port", "8700"]
+    sys.argv += ["--port", "8769"]
     main()
